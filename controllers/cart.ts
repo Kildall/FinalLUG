029971f5eacabc243976e21d7bbfb6c10248b7e8
francopigna <francopignatelli15@gmail.com>
2022-11-01 23:26:27 -0300

    agregado funcionalidad de agregar, eliminar, actualizar items

--- controllers/cart.ts
+++ controllers/cart.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express'
+import { Types } from 'mongoose'
+import Cart from '../models/Cart'
 import CartModel from '../models/Cart'
+import item from './item'
 
 export default {
     getCart: async (req: Request, res: Response) => {
@@ -38,17 +41,109 @@ export default {
         //Received item list (items) in body and cartId in params
         //TODO: Insert items from body into the cart
 
+        try {
+            const { cartId } = req.params
+            if(!cartId){
+                res.status(400).send('cartId invalido.')
+                return
+            }
+            
+            const cart = await CartModel.findById(cartId)
+
+            if(!cart){
+                res.status(404).send('cartId no encontrado.')
+                return
+            }
+
+            req.body.items.forEach((item:Types.ObjectId) => {
+                cart.items.push(item)
+            });
+
+            cart.save()
+            
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Ha ocurrido un error.')
+        }
     },
     updateItemFromCart: async (req: Request, res: Response) => {
         //Received 2 item list (oldItems, newItems) and cartId in params
         //TODO: Update items in cart with new items
+
+        try {
+            const { cartId } = req.params
+            if(!cartId){
+                res.status(400).send('cartId invalido.')
+                return
+            }
+            
+            const cart = await CartModel.findById(cartId)
+
+            if(!cart){
+                res.status(404).send('cartId no encontrado.')
+                return
+            }
+
+            req.body.newItems.forEach((item:Types.ObjectId) => {
+                cart.items.push(item)
+            });
+
+            cart.save()
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Ha ocurrido un error.')
+        }
+
     },
     deleteItemFromCart: async (req: Request, res: Response) => {
         //Received item list (items) and cartId in params
         //TODO: Delete items from list from the cart
+
+        try {
+            const { cartId } = req.params
+            if(!cartId){
+                res.status(400).send('cartId invalido.')
+                return
+            }
+            
+            const cart = await CartModel.findById(cartId)
+
+            if(!cart){
+                res.status(404).send('cartId no encontrado.')
+                return
+            }
+
+            cart.items.filter(item => req.body.items.find(item))
+
+            cart.save()
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Ha ocurrido un error.')
+        }
+
+
+
     },
     deleteCart: async (req: Request, res: Response) => {
         //Received cartId in params
         //TODO: Delete cart
+
+        const { cartId } = req.params
+        if(!cartId){
+            res.status(400).send('cartId invalido.')
+            return
+        }
+        
+        try {
+            const cart = await CartModel.findByIdAndDelete(cartId)
+            res.status(200).send(cart)
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Ha ocurrido un error.')
+        }
+
     }
 }
